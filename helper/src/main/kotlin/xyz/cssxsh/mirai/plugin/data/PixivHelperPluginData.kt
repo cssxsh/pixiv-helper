e183c00cfe48d6ce7b9799f9e6198de796e4396b
cssxsh <cssxsh@gmail.com>
2020-10-05 14:22:11 +0800

    fix: PixivHelperPluginData

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/data/PixivHelperPluginData.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/data/PixivHelperPluginData.kt
@@ -8,14 +8,20 @@ import net.mamoe.mirai.contact.Group
 
 object PixivHelperPluginData : AutoSavePluginData() {
     /**
-     * 用于存储用户关联
+     * 用于存储用户关联助手ID
      */
-    private val users: MutableMap<Long, PixivClientData> by value(mutableMapOf())
+    private val users: MutableMap<Long, Long> by value(mutableMapOf())
 
     /**
      * 用于存储群关联
      */
-    private val groups: MutableMap<Long, PixivClientData> by value(mutableMapOf())
+    private val groups: MutableMap<Long, Long> by value(mutableMapOf())
+
+
+    /**
+     * 用于存储群关联
+     */
+    private val clientData: MutableMap<Long, PixivClientData> by value(mutableMapOf())
 
     /**
      * 通过联系人获取
@@ -23,10 +29,10 @@ object PixivHelperPluginData : AutoSavePluginData() {
      * @param defaultValue 构建默认值
      */
     private fun getOrPut(contact: Contact, defaultValue: () -> PixivClientData): PixivClientData = when(contact) {
-        is User -> users.getOrPut(contact.id) { defaultValue() }
-        is Group -> groups.getOrPut(contact.id) { defaultValue() }
+        is User -> users[contact.id]?.let { clientData[it] }
+        is Group -> groups[contact.id]?.let { clientData[it] }
         else -> throw IllegalArgumentException("未知类型联系人!")
-    }
+    } ?: set(contact, defaultValue())
 
     /**
      * 操作符[] 关联 getOrPut, 默认值为 PixivClientData()
@@ -37,9 +43,14 @@ object PixivHelperPluginData : AutoSavePluginData() {
     /**
      * set 操作符[] 关联 put
      */
-    operator fun set(contact: Contact, value: PixivClientData) = when(contact) {
-        is User -> users[contact.id] = value
-        is Group -> groups[contact.id] = value
-        else -> throw IllegalArgumentException("未知类型联系人!")
+    operator fun set(contact: Contact, value: PixivClientData) = value.apply {
+        authInfo?.user?.uid?.let {
+            when(contact) {
+                is User -> users[contact.id] = it
+                is Group -> groups[contact.id] = it
+                else -> throw IllegalArgumentException("未知类型联系人!")
+            }
+            clientData[it] = this
+        }
     }
 }
