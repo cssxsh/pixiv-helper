f1a60ca0c2fa8b986a8532d1b93e790fa03b114d
cssxsh <cssxsh@gmail.com>
2020-10-08 08:03:21 +0800

    fix: PixivSearch

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivSearch.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivSearch.kt
@@ -1,26 +1,37 @@
 package xyz.cssxsh.mirai.plugin.command
 
+import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.CommandSenderOnMessage
 import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.console.command.description.CommandArgumentParser
+import net.mamoe.mirai.console.command.description.CommandArgumentParserException
+import net.mamoe.mirai.console.command.description.buildCommandArgumentContext
 import net.mamoe.mirai.message.MessageEvent
 import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.queryUrl
 import xyz.cssxsh.mirai.plugin.ImageSearcher
 import xyz.cssxsh.mirai.plugin.PixivHelperPlugin
+import kotlin.reflect.KClass
 
 @Suppress("unused")
 object PixivSearch : SimpleCommand(
     PixivHelperPlugin,
     "search", "搜索",
     description = "缓存指令",
-    prefixOptional = true
+    prefixOptional = true,
+    overrideContext = buildCommandArgumentContext {
+        Image::class with object : CommandArgumentParser<Image> {
+            override fun parse(raw: String, sender: CommandSender): Image =
+                """[A-Z-]+""".toRegex().find(raw)?.let { matchResult ->
+                    Image(matchResult.value)
+                } ?: throw CommandArgumentParserException()
+        }
+    }
 ) {
 
     @Handler
-    suspend fun CommandSenderOnMessage<MessageEvent>.handle(imageInfo: String) = runCatching {
-        """[A-Z]+""".toRegex().find(imageInfo)?.let { matchResult ->
-            ImageSearcher.getSearchResults(Image(matchResult.value).queryUrl()).maxByOrNull { it.similarity }
-        }
+    suspend fun CommandSenderOnMessage<MessageEvent>.handle(image: Image) = runCatching {
+        ImageSearcher.getSearchResults(image.queryUrl()).maxByOrNull { it.similarity }
     }.onSuccess { result ->
         quoteReply(result?.content ?: "没有搜索结果")
     }.onFailure {
