b0eed759a8f5d9b80d0f38beffb6970dbecfd09c
cssxsh <cssxsh@gmail.com>
2020-10-08 08:44:04 +0800

    fix: PixivSearch

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivSearch.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivSearch.kt
@@ -8,6 +8,8 @@ import net.mamoe.mirai.console.command.description.CommandArgumentParserExceptio
 import net.mamoe.mirai.console.command.description.buildCommandArgumentContext
 import net.mamoe.mirai.message.MessageEvent
 import net.mamoe.mirai.message.data.Image
+import net.mamoe.mirai.message.data.MessageContent
+import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.message.data.queryUrl
 import xyz.cssxsh.mirai.plugin.ImageSearcher
 import xyz.cssxsh.mirai.plugin.PixivHelperPlugin
@@ -20,19 +22,23 @@ object PixivSearch : SimpleCommand(
     prefixOptional = true,
     overrideContext = buildCommandArgumentContext {
         Image::class with object : CommandArgumentParser<Image> {
-            override fun parse(raw: String, sender: CommandSender): Image =
-                """[A-Z\-]+""".toRegex().find(raw)?.let { matchResult ->
-                    Image(matchResult.value)
-                } ?: throw CommandArgumentParserException(raw)
+            override fun parse(raw: String, sender: CommandSender): Image = throw CommandArgumentParserException(raw)
+
+            override fun parse(raw: MessageContent, sender: CommandSender): Image = when(raw) {
+                is Image ->  raw
+                else -> throw CommandArgumentParserException(raw.content)
+            }
         }
     }
 ) {
 
     @Handler
     suspend fun CommandSenderOnMessage<MessageEvent>.handle(image: Image) = runCatching {
-        ImageSearcher.getSearchResults(image.queryUrl()).maxByOrNull { it.similarity }
+        ImageSearcher.getSearchResults(image.queryUrl()).maxByOrNull { it.similarity }?.let {
+            "相似度: ${it.similarity} \n ${it.content}"
+        }
     }.onSuccess { result ->
-        quoteReply(result?.content ?: "没有搜索结果")
+        quoteReply(result ?: "没有搜索结果")
     }.onFailure {
         quoteReply(it.toString())
     }.isSuccess
