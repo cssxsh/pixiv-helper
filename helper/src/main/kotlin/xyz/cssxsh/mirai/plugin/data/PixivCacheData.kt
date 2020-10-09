5dfe35631e0189dda9f34966d072a902b10a5a89
cssxsh <cssxsh@gmail.com>
2020-10-10 00:07:18 +0800

    fix: illusts MutableSet

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/data/PixivCacheData.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/data/PixivCacheData.kt
@@ -10,7 +10,7 @@ object PixivCacheData : AutoSavePluginData("PixivCache"), PixivHelperLogger {
     /**
      * 缓存
      */
-    private val illusts: MutableList<Long> by value(mutableListOf())
+    private val illusts: MutableSet<Long> by value(mutableSetOf())
 
     val eros: MutableMap<Long, IllustInfo> by value(mutableMapOf())
 
@@ -25,9 +25,10 @@ object PixivCacheData : AutoSavePluginData("PixivCache"), PixivHelperLogger {
     }
 
     fun add(illust: IllustInfo) = synchronized(illusts) {
-        illusts.add(illust.pid)
-        logger.info("作品(${illust.pid})[${illust.title}]信息将添加, 目前共${illusts.size}条信息")
-        if (illust.isEro()) eros[illust.pid] = illust
+        if (illusts.add(illust.pid)) {
+            logger.info("作品(${illust.pid})[${illust.title}]信息将添加, 目前共${illusts.size}条信息")
+            if (illust.isEro()) eros[illust.pid] = illust
+        }
     }
 
     fun remove(illust: IllustInfo) = synchronized(illusts) {
