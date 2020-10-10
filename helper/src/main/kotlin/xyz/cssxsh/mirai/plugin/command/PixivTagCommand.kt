e2c10874f376641836feee6a8112d7c0e42c134d
cssxsh <cssxsh@gmail.com>
2020-10-11 06:20:36 +0800

    add: cmd tag

---
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivTagCommand.kt
@@ -0,0 +1,34 @@
+package xyz.cssxsh.mirai.plugin.command
+
+import net.mamoe.mirai.console.command.CommandSenderOnMessage
+import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.message.MessageEvent
+import xyz.cssxsh.mirai.plugin.PixivHelperLogger
+import xyz.cssxsh.mirai.plugin.PixivHelperPlugin
+import xyz.cssxsh.mirai.plugin.buildMessage
+import xyz.cssxsh.mirai.plugin.data.PixivCacheData
+import xyz.cssxsh.mirai.plugin.getHelper
+
+object PixivTagCommand: SimpleCommand(
+    PixivHelperPlugin,
+    "tag", "标签",
+    description = "pixiv 标签",
+    prefixOptional = true
+), PixivHelperLogger {
+
+    @Handler
+    @Suppress("unused")
+    suspend fun CommandSenderOnMessage<MessageEvent>.handle(tag: String) = getHelper().runCatching {
+        (PixivCacheData.eros + PixivCacheData.r18s).values.filter { illust ->
+            illust.tags.any {
+                tag in it.name || tag in it.translatedName ?: ""
+            }
+        }.let {
+            buildMessage(it.random())
+        }
+    }.onSuccess { list ->
+        list.forEach { quoteReply(it) }
+    }.onFailure {
+        quoteReply("读取色图失败， ${it.message}")
+    }.isSuccess
+}
