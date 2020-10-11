b2ec9f59ba7ebc57ea54f0c8d154d689e238ee4a
cssxsh <cssxsh@gmail.com>
2020-10-12 03:45:24 +0800

    add: searchTag

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivTagCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivTagCommand.kt
@@ -1,10 +1,14 @@
 package xyz.cssxsh.mirai.plugin.command
 
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.command.CommandSenderOnMessage
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.message.MessageEvent
 import xyz.cssxsh.mirai.plugin.*
 import xyz.cssxsh.mirai.plugin.data.PixivCacheData
+import xyz.cssxsh.pixiv.api.app.illustRelated
+import xyz.cssxsh.pixiv.data.app.IllustInfo
 
 object PixivTagCommand: SimpleCommand(
     PixivHelperPlugin,
@@ -13,13 +17,23 @@ object PixivTagCommand: SimpleCommand(
     prefixOptional = true
 ), PixivHelperLogger {
 
+    private val jobs : MutableList<Job> = mutableListOf()
+
+    private fun PixivHelper.searchTag(illust: IllustInfo, seedIllusts: List<IllustInfo>) = launch {
+        illustRelated(illust.pid, seedIllusts.map { it.pid }).illusts.forEach {
+            PixivCacheData.add(it)
+        }
+    }.also {
+        jobs.add(it)
+    }
+
     @Handler
     @Suppress("unused")
     suspend fun CommandSenderOnMessage<MessageEvent>.handle(tag: String) = getHelper().runCatching {
         PixivCacheData.eros.values.filter { illust ->
             tag in illust.title || illust.tags.any { tag in it.name || tag in it.translatedName ?: "" }
-        }.let {
-            buildMessage(it.random())
+        }.let { list ->
+            buildMessage(list.random().also { searchTag(it, list) })
         }
     }.onSuccess { list ->
         list.forEach { quoteReply(it) }
