1bf97f41eebc966e6aa20088e130bef635d1a609
cssxsh <cssxsh@gmail.com>
2020-10-14 12:18:12 +0800

    fix: tozip

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivCacheCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivCacheCommand.kt
@@ -354,8 +354,8 @@ object PixivCacheCommand : CompositeCommand(
 
     @SubCommand
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun ConsoleCommandSender.tozip(uid: Long) = withContext(Dispatchers.IO) {
-        ZipOutputStream(File("~/${uid}.zip").apply { createNewFile() }.outputStream()).use { zipOutputStream ->
+    suspend fun ConsoleCommandSender.tozip(uid: Long, path: String) = withContext(Dispatchers.IO) {
+        ZipOutputStream(File(path).apply { createNewFile() }.outputStream()).use { zipOutputStream ->
             zipOutputStream.setLevel(BEST_COMPRESSION)
             BufferedOutputStream(zipOutputStream).use { buffer ->
                 PixivCacheData.caches().values.filter {
