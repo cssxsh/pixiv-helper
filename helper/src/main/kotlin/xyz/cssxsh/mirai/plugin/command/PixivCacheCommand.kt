4f3b5a112672fb7b7a03eab3e85ad96d08f06860
cssxsh <cssxsh@gmail.com>
2020-10-14 13:51:27 +0800

    fix: tozip

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivCacheCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivCacheCommand.kt
@@ -370,7 +370,7 @@ object PixivCacheCommand : CompositeCommand(
                 logger.verbose("共${it.size} 个作品将写入文件")
             }.forEach { info ->
                 PixivHelperSettings.imagesFolder(info.pid).listFiles()?.forEach { file ->
-                    zipOutputStream.putNextEntry(ZipEntry("[${info.pid}](${info.title.replace("/", "///")})/${file.name}").apply {
+                    zipOutputStream.putNextEntry(ZipEntry("[${info.pid}](${info.getFullWidthTitle()})/${file.name}").apply {
                         creationTime = FileTime.fromMillis(info.createDate.utc.unixMillisLong)
                         lastModifiedTime = FileTime.fromMillis(info.createDate.utc.unixMillisLong)
                     })
