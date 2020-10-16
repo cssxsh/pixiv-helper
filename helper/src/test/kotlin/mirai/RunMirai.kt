7865994955c1d36b407e89315a88d1804a2c1245
cssxsh <cssxsh@gmail.com>
2020-10-16 09:15:24 +0800

    fix: subscribeEvent

--- helper/src/test/kotlin/mirai/RunMirai.kt
+++ helper/src/test/kotlin/mirai/RunMirai.kt
@@ -2,9 +2,15 @@ package mirai
 
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
+import io.ktor.client.features.*
+import io.ktor.client.features.compression.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.util.*
 import io.ktor.utils.io.core.*
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.runBlocking
@@ -58,6 +64,40 @@ object RunMirai {
         }
     }
 
+    private suspend fun getAmr(text: String) = HttpClient(OkHttp) {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer()
+        }
+        BrowserUserAgent()
+        ContentEncoding {
+            gzip()
+            deflate()
+            identity()
+        }
+    }.use { client ->
+        logger.verbose("开始tts $text")
+        val file = client.get<ByteArray>("https://fanyi.baidu.com/gettts") {
+            parameter("lan", "zh")
+            parameter("text", text)
+            parameter("spd", 5)
+            parameter("source", "web")
+        }
+        File("tts.mp3").writeBytes(file)
+        logger.verbose("开始mp3(${file.size}) -> amr")
+        val json = client.submitFormWithBinaryData<String>(
+            url = "https://s19.aconvert.com/convert/convert-batch.php",
+            formData = formData {
+                append(key = "file", filename = "blob", size = file.size.toLong(), contentType = ContentType.Audio.MPEG) {
+                    writeFully(file)
+                }
+                append(key = "targetformat", value = "amr")
+            }
+        )
+        logger.verbose("转换结果: $json")
+        val filename = Json.parseToJsonElement(json).jsonObject["filename"]!!.jsonPrimitive.content
+        client.get<ByteArray>("https://s19.aconvert.com/convert/p3r68-cdx67/${filename}")
+    }
+
     private fun MiraiConsole.subscribeEvent() = apply {
         subscribeAlways<NewFriendRequestEvent> {
             accept()
@@ -72,35 +112,7 @@ object RunMirai {
             """.+爬""".toRegex() matchingReply { result ->
                 File(".").resolve("${result.value}.amr").apply {
                     if (canRead().not()) {
-                        HttpClient(OkHttp).use { client ->
-                            logger.verbose("开始tts")
-                            val file = client.get<ByteArray>("https://fanyi.baidu.com/gettts") {
-                                parameter("lan", "zh")
-                                parameter("text", result.value)
-                                parameter("spd", 5)
-                                parameter("source", "web")
-                            }
-                            logger.verbose("开始mp3 -> amr")
-                            val text = client.post<String>("https://s19.aconvert.com/convert/convert-batch.php") {
-                                body = MultiPartFormDataContent(formData {
-                                    append(key = "file", filename = "tts.mp3", contentType = ContentType.Audio.MPEG) {
-                                        writeFully(file)
-                                    }
-                                    append(key = "targetformat", value = "amr")
-                                    append(key = "audiobitratetype", value = 0)
-                                    append(key = "customaudiobitrate", value = "")
-                                    append(key = "audiosamplingtype", value = 0)
-                                    append(key = "customaudiosampling", value = "")
-                                    append(key = "code", value = 82000)
-                                    append(key = "filelocation", value = "local")
-                                })
-                            }
-                            logger.verbose("转换结果: $text")
-                            val filename = Json.parseToJsonElement(text).jsonObject["filename"]!!.jsonPrimitive.content
-                            client.get<ByteArray>("https://s19.aconvert.com/convert/p3r68-cdx67/${filename}")
-                        }.let {
-                            writeBytes(it)
-                        }
+                        writeBytes(getAmr(result.value))
                     }
                 }.let {
                     group.uploadVoice(it.inputStream())
