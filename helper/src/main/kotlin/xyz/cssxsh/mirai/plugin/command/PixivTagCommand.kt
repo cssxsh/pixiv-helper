c5b97c10875f2abec13d09d275bfa50f3082d73d
cssxsh <cssxsh@gmail.com>
2020-10-17 07:10:43 +0800

    fix: random

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivTagCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivTagCommand.kt
@@ -84,14 +84,16 @@ object PixivTagCommand: SimpleCommand(
         if (jobs.none { it.isActive }) {
             jobs.clear()
             PixivCacheData.caches().values.filter { info ->
-                info.isR18().not()
-            }.filter { info ->
                  tag in info.title || info.tags.any { tag in it.name || tag in it.translatedName ?: "" }
             }.let { list ->
                 logger.verbose("根据TAG: $tag 在缓存中找到${list.size}个作品")
-                buildMessage(list.random().also { info ->
+                list.filter { info ->
+                    info.isR18().not()
+                }.random().also { info ->
                     if (list.size < PixivHelperSettings.maxTagCount) addRelated(info.pid, list.map { it.pid })
-                })
+                }.let {
+                    buildMessage(it)
+                }
             }
         } else {
             listOf(PlainText("技能冷却中"))
