86677996c1c71d054aa1ebe56236efbd5b503200
cssxsh <cssxsh@gmail.com>
2020-10-20 02:41:46 +0800

    debug: subscribeGroupMessages

--- helper/src/test/kotlin/mirai/RunMirai.kt
+++ helper/src/test/kotlin/mirai/RunMirai.kt
@@ -1,22 +1,7 @@
 package mirai
 
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.features.*
-import io.ktor.client.features.compression.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
-import io.ktor.client.request.*
-import io.ktor.client.request.forms.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.util.*
-import io.ktor.utils.io.core.*
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
 import mirai.TTS.getAmrFile
 import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.terminal.ConsoleTerminalExperimentalApi
@@ -27,10 +12,8 @@ import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent
 import net.mamoe.mirai.event.events.NewFriendRequestEvent
 import net.mamoe.mirai.event.subscribeAlways
 import net.mamoe.mirai.event.subscribeGroupMessages
-import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
-import kotlin.io.use
 
 
 @ConsoleExperimentalApi
@@ -68,9 +51,6 @@ object RunMirai {
             accept()
         }
         subscribeGroupMessages {
-            atBot {
-                quoteReply("部分指令需要好友私聊，已添加自动好友验证\n有事请联系：QQ: 1438159989")
-            }
             """(?:说：|say:)(.+)""".toRegex() matchingReply { result ->
                 val text = if (result.groupValues[1].length < 128) result.groupValues[1] else "太长不说"
                 group.uploadVoice(getAmrFile(text).inputStream())
