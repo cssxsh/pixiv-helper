7741819ac71186e4e93f40d3ac940bddcd18357a
cssxsh <cssxsh@gmail.com>
2020-10-19 08:44:22 +0800

    fix: PixivFollowCommand

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivFollowCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivFollowCommand.kt
@@ -17,25 +17,26 @@ object PixivFollowCommand : CompositeCommand(
     prefixOptional = true
 ), PixivHelperLogger {
 
+    private suspend fun PixivHelper.getFollowed(uid: Long, maxNum: Long = 10_000): Set<Long> = buildList {
+        (0L until maxNum step AppApi.PAGE_SIZE).forEach { offset ->
+            runCatching {
+                userFollowing(uid = uid, offset = offset).userPreviews.map { it.user.id }
+            }.onSuccess {
+                if (it.isEmpty()) return@buildList
+                add(it)
+                logger.verbose("加载关注用户作品预览第${offset / 30}页{${it.size}}成功")
+            }.onFailure {
+                logger.warning("加载关注用户作品预览第${offset / 30}页失败", it)
+            }
+        }
+    }.flatten().toSet()
+
     /**
      * 关注色图作者
      */
     @SubCommand
-    suspend fun CommandSenderOnMessage<MessageEvent>.good() = getHelper().runCatching {
-         val authInfo = getAuthInfo()
-         val followed = buildList {
-            (0L until 10_000 step AppApi.PAGE_SIZE).forEach { offset ->
-                runCatching {
-                    userFollowing(uid = authInfo.user.uid, offset = offset).userPreviews.map { it.user.id }
-                }.onSuccess {
-                    if (it.isEmpty()) return@buildList
-                    add(it)
-                    logger.verbose("加载关注用户作品预览第${offset / 30}页{${it.size}}成功")
-                }.onFailure {
-                    logger.warning("加载关注用户作品预览第${offset / 30}页失败", it)
-                }
-            }
-        }.flatten().toSet()
+    suspend fun CommandSenderOnMessage<MessageEvent>.ero() = getHelper().runCatching {
+         val followed = getFollowed(uid = getAuthInfo().user.uid)
 
         PixivCacheData.caches().values.filter { info ->
             info.isEro()
@@ -48,12 +49,12 @@ object PixivFollowCommand : CompositeCommand(
         }.run {
             size to count { uid ->
                 runCatching {
+                    delay(30.secondsToMillis)
                     userFollowAdd(uid)
                 }.onSuccess {
-                    logger.info("用户(${authInfo.user.name})添加关注(${uid})成功, $it")
-                    delay(3.secondsToMillis)
+                    logger.info("用户(${getAuthInfo().user.name})[${getAuthInfo().user.uid}]添加关注(${uid})成功, $it")
                 }.onFailure {
-                    logger.warning("用户(${authInfo.user.name})添加关注(${uid})失败", it)
+                    logger.warning("用户(${getAuthInfo().user.name})[${getAuthInfo().user.uid}]添加关注(${uid})失败", it)
                 }.isSuccess
             }
         }
