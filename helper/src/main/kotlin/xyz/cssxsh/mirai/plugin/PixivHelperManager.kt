c0644fdc5e727b9794e547fab4e15a8b24ba6876
cssxsh <cssxsh@gmail.com>
2020-10-21 03:45:18 +0800

    add: Delegates

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperManager.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperManager.kt
@@ -1,14 +1,14 @@
 package xyz.cssxsh.mirai.plugin
 
-import com.soywiz.klock.wrapped.WDateTime
 import com.soywiz.klock.wrapped.WDateTimeTz
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.contact.User
 import xyz.cssxsh.pixiv.data.AuthResult
 
-object PixivHelperManager: PixivHelperLogger {
+object PixivHelperManager : PixivHelperLogger {
     /**
      * 用于存储用户关联的助手
      */
@@ -19,25 +19,34 @@ object PixivHelperManager: PixivHelperLogger {
      */
     private val groups: MutableMap<Long, PixivHelper> = mutableMapOf()
 
+    val userAuthInfos: MutableMap<Long, AuthResult.AuthInfo?> = mutableMapOf()
 
-    /**
-     * 用于存储认证消息
-     */
-    var authInfo: AuthResult.AuthInfo? = null
+    val userExpiresTimes: MutableMap<Long, WDateTimeTz> = mutableMapOf()
 
-    /**
-     * 信息
-     */
-    var expiresTime: WDateTimeTz = WDateTime.now().local
+    var defaultAuthInfos: AuthResult.AuthInfo? = null
+
+    var defaultExpiresTimes: WDateTimeTz = WDateTimeTz.nowLocal()
 
     /**
      * 通过联系人获取
      * @param contact 助手的联系人
      * @param defaultValue 构建默认值
      */
-    fun getOrPut(contact: Contact, defaultValue: () -> PixivHelper): PixivHelper = when(contact) {
-        is User -> synchronized(users) { users.getOrPut(contact.id) { defaultValue() } }
-        is Group -> synchronized(groups) { groups.getOrPut(contact.id) { defaultValue() } }
+    fun getOrPut(contact: Contact, defaultValue: () -> PixivHelper): PixivHelper = when (contact) {
+        is User -> synchronized(users) {
+            users.getOrPut(contact.id) {
+                defaultValue().apply {
+                    launch {
+                        reply("目前测试私聊模式中使用用户自己的账户，请使用 pixiv login <uid> <password> 指令尝试登陆")
+                    }
+                }
+            }
+        }
+        is Group -> synchronized(groups) {
+            groups.getOrPut(contact.id) {
+                defaultValue()
+            }
+        }
         else -> throw IllegalAccessException("未知类型联系人!")
     }
 
@@ -50,17 +59,18 @@ object PixivHelperManager: PixivHelperLogger {
     /**
      * set 操作符[] 关联 put
      */
-    operator fun set(contact: Contact, value: PixivHelper) = when(contact) {
+    operator fun set(contact: Contact, value: PixivHelper) = when (contact) {
         is User -> synchronized(users) { users[contact.id] = value }
         is Group -> synchronized(groups) { groups[contact.id] = value }
         else -> throw IllegalAccessException("未知类型联系人!")
     }
 
-    fun remove(contact: Contact) = when(contact) {
+    fun remove(contact: Contact) = when (contact) {
         is User -> synchronized(users) { users.remove(contact.id) }
         is Group -> synchronized(groups) { groups.remove(contact.id) }
         else -> throw IllegalAccessException("未知类型联系人!")
     }?.apply {
         cancel()
+
     }
 }
--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/data/PixivConfigData.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/data/PixivConfigData.kt
@@ -1,15 +1,20 @@
 package xyz.cssxsh.mirai.plugin.data
 
-import net.mamoe.mirai.console.data.AutoSavePluginData
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 import xyz.cssxsh.pixiv.client.PixivConfig
 
-object PixivConfigData : AutoSavePluginData("PixivConfig") {
+object PixivConfigData : AutoSavePluginConfig("PixivConfig") {
 
     /**
-     * 助手配置
+     * 默认助手配置
      */
-    var config: PixivConfig by value()
+    var default: PixivConfig by value()
+
+    /**
+     * 特定助手配置
+     */
+    val configs: MutableMap<Long, PixivConfig> by value()
 
     /**
      * 作品信息是否为简单构造
