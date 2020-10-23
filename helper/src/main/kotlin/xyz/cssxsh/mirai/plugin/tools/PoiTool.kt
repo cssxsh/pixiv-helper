249ba8ee90621f2acc7ede32f5f7575534517bb1
cssxsh <cssxsh@gmail.com>
2020-10-23 22:27:36 +0800

    fix: PoiTool

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/tools/PoiTool.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/tools/PoiTool.kt
@@ -1,12 +1,13 @@
 package xyz.cssxsh.mirai.plugin.tools
 
-import com.soywiz.klock.DateFormat
+import com.soywiz.klock.jvm.toDate
 import com.soywiz.klock.wrapped.WDateTimeTz
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Sheet
-import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory
+import org.apache.poi.xssf.usermodel.*
+import xyz.cssxsh.mirai.plugin.PixivHelperLogger
 import xyz.cssxsh.mirai.plugin.PixivHelperPlugin
 import xyz.cssxsh.mirai.plugin.data.BaseInfo
 import xyz.cssxsh.mirai.plugin.data.PixivCacheData
@@ -16,13 +17,13 @@ import xyz.cssxsh.mirai.plugin.isR18
 import java.io.File
 
 @Suppress("unused")
-object PoiTool {
+object PoiTool: PixivHelperLogger {
     private fun xlsxFile() = File(
         PixivHelperSettings.zipFolder,
         "PixivData(${WDateTimeTz.nowLocal().format("yyyy-MM-dd-HH-mm-ss")}).xlsx"
     )
 
-    private val headerTexts = listOf(
+    private val PIXIV_CACHE_DATA_HEADER = listOf(
         "PID",
         "TITLE",
         "CREATE_DATE",
@@ -37,37 +38,43 @@ object PoiTool {
         "TOTAL_BOOKMARKS"
     )
 
-    private fun Row.writeInfo(info: BaseInfo) {
-        createCell(headerTexts.indexOf("PID")).setCellValue(info.pid.toDouble())
-        createCell(headerTexts.indexOf("TITLE")).setCellValue(info.title)
-        createCell(headerTexts.indexOf("CREATE_DATE")).setCellValue(info.createDate.toString(DateFormat.FORMAT_DATE))
-        createCell(headerTexts.indexOf("PAGE_COUNT")).setCellValue(info.pageCount.toDouble())
-        createCell(headerTexts.indexOf("SANITY_LEVEL")).setCellValue(info.sanityLevel.toDouble())
-        createCell(headerTexts.indexOf("TYPE")).setCellValue(info.type.name)
-        createCell(headerTexts.indexOf("IS_R18")).setCellValue(info.isR18())
-        createCell(headerTexts.indexOf("WIDTH")).setCellValue(info.width.toDouble())
-        createCell(headerTexts.indexOf("HEIGHT")).setCellValue(info.height.toDouble())
-        createCell(headerTexts.indexOf("USER_ID")).setCellValue(info.uid.toDouble())
-        createCell(headerTexts.indexOf("USER_NAME")).setCellValue(info.uname)
-        createCell(headerTexts.indexOf("TOTAL_BOOKMARKS")).setCellValue(info.totalBookmarks.toDouble())
+    private fun XSSFRow.writeInfo(info: BaseInfo, dateTimeStyle: XSSFCellStyle) {
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("PID")).setCellValue(info.pid.toDouble())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("TITLE")).setCellValue(info.title)
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("CREATE_DATE")).apply {
+            setCellValue(info.createDate.local.toDate())
+            cellStyle = dateTimeStyle
+        }
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("PAGE_COUNT")).setCellValue(info.pageCount.toDouble())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("SANITY_LEVEL")).setCellValue(info.sanityLevel.toDouble())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("TYPE")).setCellValue(info.type.name)
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("IS_R18")).setCellValue(info.isR18())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("WIDTH")).setCellValue(info.width.toDouble())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("HEIGHT")).setCellValue(info.height.toDouble())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("USER_ID")).setCellValue(info.uid.toDouble())
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("USER_NAME")).setCellValue(info.uname)
+        createCell(PIXIV_CACHE_DATA_HEADER.indexOf("TOTAL_BOOKMARKS")).setCellValue(info.totalBookmarks.toDouble())
     }
 
-    private fun Sheet.writeInfos() = apply {
+    private fun XSSFSheet.writeInfos(dateTimeStyle: XSSFCellStyle) = apply {
+        setDefaultColumnStyle(PIXIV_CACHE_DATA_HEADER.indexOf("CREATE_DATE"), dateTimeStyle)
+        PixivCacheData.caches().values.forEachIndexed { row, info ->
+            createRow(row + 1).writeInfo(info, dateTimeStyle)
+        }
         createRow(0).apply {
-            headerTexts.forEachIndexed { column, text ->
+            PIXIV_CACHE_DATA_HEADER.forEachIndexed { column, text ->
                 createCell(column).setCellValue(text)
+                autoSizeColumn(column)
             }
         }
-        PixivCacheData.caches().values.forEachIndexed { row, info ->
-            createRow(row + 1).writeInfo(info)
-        }
+        setDefaultColumnStyle(PIXIV_CACHE_DATA_HEADER.indexOf("CREATE_DATE"), workbook.createCellStyle().apply {
+            dataFormat = workbook.createDataFormat().getFormat("yyyy-mm-dd hh:mm:ss")
+        })
     }
 
-    private fun Sheet.writeTags() = apply {
-        createRow(0).apply {
-            createCell(0).setCellValue("TAG")
-            createCell(1).setCellValue("TOTAL")
-        }
+    private val PIXIV_TAG_DATA_HEADER = listOf("TAG", "TOTAL")
+
+    private fun XSSFSheet.writeTags() = apply {
         buildMap<String, Int> {
             PixivCacheData.caches().values.flatMap {
                 it.tags
@@ -81,38 +88,52 @@ object PoiTool {
             }
         }.entries.forEachIndexed { row, (tag, num) ->
             createRow(row + 1).apply {
-                createCell(0).setCellValue(tag)
-                createCell(1).setCellValue(num.toDouble())
+                createCell(PIXIV_TAG_DATA_HEADER.indexOf("TAG")).setCellValue(tag)
+                createCell(PIXIV_TAG_DATA_HEADER.indexOf("TOTAL")).setCellValue(num.toDouble())
+            }
+        }
+        createRow(0).apply {
+            PIXIV_TAG_DATA_HEADER.forEachIndexed { column, text ->
+                createCell(column).setCellValue(text)
+                autoSizeColumn(column)
             }
         }
     }
 
-    private fun Sheet.writeStatistical() = apply {
-        createRow(0).apply {
-            createCell(0).setCellValue("QQ")
-            createCell(1).setCellValue("ERO")
-            createCell(2).setCellValue("TAG")
-        }
+    private val PIXIV_STATISTICAL_DATA_HEADER = listOf("QQ", "ERO", "TAG")
+
+    private fun XSSFSheet.writeStatistical() = apply {
         PixivStatisticalData.getMap().entries.forEachIndexed { row, (qq, data) ->
             createRow(row + 1).apply {
-                createCell(0).setCellValue(qq.toDouble())
-                createCell(1).setCellValue(data.eroCount.toDouble())
+                createCell(PIXIV_STATISTICAL_DATA_HEADER.indexOf("QQ")).setCellValue(qq.toDouble())
+                createCell(PIXIV_STATISTICAL_DATA_HEADER.indexOf("ERO")).setCellValue(data.eroCount.toDouble())
                 data.tagCount.entries.forEachIndexed { row, (tag, total) ->
                     createCell(row + 2).setCellValue("$tag: $total")
                 }
+            }
+        }
+        createRow(0).apply {
+            PIXIV_STATISTICAL_DATA_HEADER.forEachIndexed { column, text ->
+                createCell(column).setCellValue(text)
+                autoSizeColumn(column)
             }
         }
     }
 
     fun saveCacheToXlsxAsync() = PixivHelperPlugin.async(Dispatchers.IO) {
         XSSFWorkbookFactory.createWorkbook().use { workbook ->
-            workbook.createSheet("PIXIV_CACHE_DATA").writeInfos()
-            workbook.createSheet("PIXIV_TAG_DATA").writeTags()
-            workbook.createSheet("PIXIV_STATISTICAL_DATA").writeStatistical()
+            workbook.apply {
+                createSheet("PIXIV_CACHE_DATA").writeInfos(createCellStyle().apply {
+                    dataFormat = workbook.createDataFormat().getFormat("yyyy-mm-dd hh:mm:ss")
+                })
+                createSheet("PIXIV_TAG_DATA").writeTags()
+                createSheet("PIXIV_STATISTICAL_DATA").writeStatistical()
+            }
             xlsxFile().apply {
                 outputStream().use {
                     workbook.write(it)
                 }
+                logger.verbose("数据将保存至${absolutePath}")
             }
         }
     }
