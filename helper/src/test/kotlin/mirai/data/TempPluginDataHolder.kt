d499d1150f1d3c62c36b0348a536ced25b646433
cssxsh <cssxsh@gmail.com>
2020-10-24 14:04:41 +0800

    add: BiliBiliCommand

--- helper/src/test/kotlin/mirai/data/TempPluginDataHolder.kt
+++ helper/src/test/kotlin/mirai/data/TempPluginDataHolder.kt
@@ -1,10 +1,20 @@
 package mirai.data
 
-import net.mamoe.mirai.console.data.PluginDataHolder
+import kotlinx.coroutines.CoroutineName
+import net.mamoe.mirai.console.data.AutoSavePluginDataHolder
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.utils.hoursToMillis
+import net.mamoe.mirai.utils.minutesToMillis
+import kotlin.coroutines.CoroutineContext
 
 @ConsoleExperimentalApi
-object TempPluginDataHolder : PluginDataHolder {
+object TempPluginDataHolder : AutoSavePluginDataHolder {
+    @ConsoleExperimentalApi
+    override val autoSaveIntervalMillis: LongRange
+        get() = 3.minutesToMillis..30.hoursToMillis
+
+    override val coroutineContext: CoroutineContext = CoroutineName("TempPluginDataHolder")
+
     @ConsoleExperimentalApi
     override val dataHolderName: String
         get() = "TempPluginDataHolder"
