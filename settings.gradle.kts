7e6a5d231324b1ede01a0d0221b4b033abe9b809
cssxsh <cssxsh@gmail.com>
2020-10-31 09:52:47 +0800

    fix: mavenLocal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,8 @@
 pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "com.github.johnrengelman.shadow" -> useModule("com.github.jengelman.gradle.plugins:shadow:${requested.version}")
-                "org.jetbrains.kotlin.jvm" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-
     repositories {
+        mavenLocal()
         maven(url = "https://maven.aliyun.com/repository/releases")
         maven(url = "https://mirrors.huaweicloud.com/repository/maven")
-        mavenLocal()
         gradlePluginPortal()
         // bintray
         maven(url = "https://bintray.proxy.ustclug.org/kotlin/kotlin-eap")
