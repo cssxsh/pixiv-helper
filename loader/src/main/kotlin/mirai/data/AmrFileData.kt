03e705ae06e899964d118130d80ee78806b2b656
cssxsh <cssxsh@gmail.com>
2020-11-19 14:33:56 +0800

    fix: AmrFileData

--- loader/src/main/kotlin/mirai/data/AmrFileData.kt
+++ loader/src/main/kotlin/mirai/data/AmrFileData.kt
@@ -1,8 +1,15 @@
 package mirai.data
 
 import net.mamoe.mirai.console.data.AutoSavePluginData
+import net.mamoe.mirai.console.data.ValueName
 import net.mamoe.mirai.console.data.value
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 
 object AmrFileData : AutoSavePluginData("AmrFile") {
+
+    @ConsoleExperimentalApi
+    override fun shouldPerformAutoSaveWheneverChanged(): Boolean = false
+
+    @ValueName("files")
     val files: MutableMap<String, String> by value(mutableMapOf())
 }
