09bfc6a54fd95d9d616663147bd57f948157c1e4
cssxsh <cssxsh@gmail.com>
2020-11-19 14:34:39 +0800

    add: rootPath

--- loader/src/main/kotlin/mirai/RunMirai.kt
+++ loader/src/main/kotlin/mirai/RunMirai.kt
@@ -15,14 +15,15 @@ import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent
 import net.mamoe.mirai.event.events.NewFriendRequestEvent
 import net.mamoe.mirai.event.subscribeAlways
-import java.nio.file.Path
 import java.nio.file.Paths
 
 @ConsoleExperimentalApi
 @ConsoleTerminalExperimentalApi
 object RunMirai {
 
-    private fun miraiConsoleImpl(rootPath: Path) = MiraiConsoleImplementationTerminal(
+    private val rootPath = Paths.get(".").toAbsolutePath()
+
+    private fun miraiConsoleImpl() = MiraiConsoleImplementationTerminal(
         rootPath = rootPath,
         dataStorageForJvmPluginLoader = JsonPluginDataStorage(rootPath.resolve("data"), false),
         dataStorageForBuiltIns = JsonPluginDataStorage(rootPath.resolve("data"), false),
@@ -33,8 +34,8 @@ object RunMirai {
     @JvmStatic
     fun main(args: Array<String>): Unit = runBlocking {
         // 默认在 /test 目录下运行
-        MiraiConsoleTerminalLoader.parse(args, exitProcess = true)
-        MiraiConsoleTerminalLoader.startAsDaemon(miraiConsoleImpl(Paths.get(".").toAbsolutePath()))
+        MiraiConsoleTerminalLoader.parse(args = args, exitProcess = true)
+        MiraiConsoleTerminalLoader.startAsDaemon(miraiConsoleImpl())
         MiraiConsole.setCustomize()
         try {
             MiraiConsole.job.join()
