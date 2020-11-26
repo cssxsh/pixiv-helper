9e692b5770549c29f6fb5fae2361aa4d1b8c5bbd
cssxsh <cssxsh@gmail.com>
2020-11-27 03:27:23 +0800

    add: PixivZipCommand

---
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivZipCommand.kt
@@ -0,0 +1,44 @@
+package xyz.cssxsh.mirai.plugin.command
+
+import kotlinx.coroutines.Deferred
+import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.console.command.ConsoleCommandSender
+import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import xyz.cssxsh.mirai.plugin.PixivHelperLogger
+import xyz.cssxsh.mirai.plugin.PixivHelperPlugin
+import xyz.cssxsh.mirai.plugin.data.PixivCacheData
+import xyz.cssxsh.mirai.plugin.tools.Zipper
+import java.io.File
+
+@Suppress("unused")
+object PixivZipCommand : CompositeCommand(
+    owner = PixivHelperPlugin,
+    "cache",
+    description = "压缩指令"
+), PixivHelperLogger {
+
+    @ExperimentalCommandDescriptors
+    @ConsoleExperimentalApi
+    override val prefixOptional: Boolean = true
+
+    private var compressJob: Deferred<File>? = null
+
+    private var backupJob: Deferred<List<File>>? = null
+
+    @SubCommand
+    fun ConsoleCommandSender.zip(uid: Long) {
+        check(compressJob?.isActive != true) { "正在压缩中, ${compressJob}..." }
+        PixivCacheData.caches().values.filter {
+            it.uid == uid
+        }.let {
+            compressJob = Zipper.compressAsync(it, "USER[${uid}]")
+        }
+    }
+
+    @SubCommand
+    fun ConsoleCommandSender.backup() {
+        check(backupJob?.isActive != true) { "正在压缩中, ${backupJob}..." }
+        backupJob = Zipper.backupAsync()
+    }
+}
