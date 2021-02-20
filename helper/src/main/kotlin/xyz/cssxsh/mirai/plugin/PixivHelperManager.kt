bae002a7c68adfd14a66b9130e118c377ff11914
cssxsh <cssxsh@gmail.com>
2021-02-21 05:12:43 +0800

    fix: PixivHelperManager

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperManager.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperManager.kt
@@ -2,66 +2,30 @@ package xyz.cssxsh.mirai.plugin
 
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
-import net.mamoe.mirai.contact.Contact
-import net.mamoe.mirai.contact.Group
-import net.mamoe.mirai.contact.User
+import net.mamoe.mirai.contact.*
 
-object PixivHelperManager {
-    /**
-     * 用于存储用户关联的助手
-     */
-    private val users: MutableMap<Long, PixivHelper> = mutableMapOf()
+object PixivHelperManager : LinkedHashMap<Contact, PixivHelper>() {
 
-    /**
-     * 用于存储群关联的助手
-     */
-    private val groups: MutableMap<Long, PixivHelper> = mutableMapOf()
+    override fun get(key: Contact): PixivHelper = super.get(key) ?: PixivHelper(key).also { put(key, it) }
 
-    /**
-     * 通过联系人获取
-     * @param contact 助手的联系人
-     * @param defaultValue 构建默认值
-     */
-    fun getOrPut(contact: Contact, defaultValue: () -> PixivHelper): PixivHelper = when (contact) {
-        is User -> synchronized(users) {
-            users.getOrPut(contact.id) {
-                defaultValue().apply {
-                    launch {
-                        sign {
-                            "目前测试私聊模式中使用用户自己的账户，请使用 pixiv login <uid> <password> 指令尝试登陆"
-                        }
-                    }
+    override fun put(key: Contact, value: PixivHelper) = super.put(key, value).also { helper ->
+        if (helper != null && key is User) {
+            value.launch {
+                value.sign {
+                    "目前测试私聊模式中使用用户自己的账户，请使用 pixiv login <uid> <password> 指令尝试登陆"
                 }
             }
         }
-        is Group -> synchronized(groups) {
-            groups.getOrPut(contact.id) {
-                defaultValue()
-            }
-        }
-        else -> throw IllegalAccessException("未知类型联系人!")
     }
 
-    /**
-     * 操作符[] 关联 getOrPut, 默认值为 PixivClientData()
-     * @see [getOrPut]
-     */
-    operator fun get(contact: Contact): PixivHelper = getOrPut(contact) { PixivHelper(contact) }
-
-    /**
-     * set 操作符[] 关联 put
-     */
-    operator fun set(contact: Contact, value: PixivHelper) = when (contact) {
-        is User -> synchronized(users) { users[contact.id] = value }
-        is Group -> synchronized(groups) { groups[contact.id] = value }
-        else -> throw IllegalAccessException("未知类型联系人!")
-    }
-
-    fun remove(contact: Contact) = when (contact) {
-        is User -> synchronized(users) { users.remove(contact.id) }
-        is Group -> synchronized(groups) { groups.remove(contact.id) }
-        else -> throw IllegalAccessException("未知类型联系人!")
-    }?.apply {
+    override fun remove(key: Contact) = super.remove(key)?.apply {
         cancel()
+    }
+
+    override fun clear() = synchronized(size) {
+        forEach { (_, helper) ->
+            helper.cancel()
+        }
+        super.clear()
     }
 }
