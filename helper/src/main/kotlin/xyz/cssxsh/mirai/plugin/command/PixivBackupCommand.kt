982efb7c2a14d726fe29de69ba7ba55c26100623
cssxsh <cssxsh@gmail.com>
2021-02-22 17:08:44 +0800

    style: PixivBackupCommand

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivBackupCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivBackupCommand.kt
@@ -1,7 +1,7 @@
 package xyz.cssxsh.mirai.plugin.command
 
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.ConsoleCommandSender
@@ -9,7 +9,6 @@ import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import xyz.cssxsh.mirai.plugin.*
 import xyz.cssxsh.mirai.plugin.tools.PixivZipper
-import java.io.File
 
 @Suppress("unused")
 object PixivBackupCommand : CompositeCommand(
@@ -22,22 +21,22 @@ object PixivBackupCommand : CompositeCommand(
     @ConsoleExperimentalApi
     override val prefixOptional: Boolean = true
 
-    private var compressJob: Deferred<File>? = null
+    private var userJob: Job? = null
 
-    private var backupJob: Deferred<List<File>>? = null
+    private var dataJob: Job? = null
 
     @SubCommand
     fun ConsoleCommandSender.user(uid: Long) {
-        check(compressJob?.isActive != true) { "正在压缩中, ${compressJob}..." }
-        compressJob = PixivHelperPlugin.async(Dispatchers.IO) {
+        check(userJob?.isActive != true) { "正在压缩中, ${userJob}..." }
+        userJob = PixivHelperPlugin.async(Dispatchers.IO) {
             PixivZipper.compressArtWorks(useArtWorkInfoMapper { it.userArtWork(uid) }, "USER[${uid}]")
         }
     }
 
     @SubCommand
-    fun ConsoleCommandSender.backup() {
-        check(backupJob?.isActive != true) { "正在压缩中, ${backupJob}..." }
-        backupJob = PixivHelperPlugin.async(Dispatchers.IO) {
+    fun ConsoleCommandSender.data() {
+        check(dataJob?.isActive != true) { "正在压缩中, ${dataJob}..." }
+        dataJob = PixivHelperPlugin.async(Dispatchers.IO) {
             PixivZipper.backupData()
         }
     }
