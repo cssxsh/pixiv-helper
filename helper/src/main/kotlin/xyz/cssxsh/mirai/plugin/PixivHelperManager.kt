3a01865f26354c5c38782f3be8df41a0c142e553
cssxsh <cssxsh@gmail.com>
2021-02-23 06:13:49 +0800

    fix: PixivHelperManager put

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperManager.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperManager.kt
@@ -9,7 +9,8 @@ object PixivHelperManager : LinkedHashMap<Contact, PixivHelper>() {
     override fun get(key: Contact): PixivHelper = super.get(key) ?: PixivHelper(key).also { put(key, it) }
 
     override fun put(key: Contact, value: PixivHelper) = super.put(key, value).also { helper ->
-        if (helper != null && key is User) {
+        helper?.cancel()
+        if (key is User) {
             value.launch {
                 value.sign {
                     "目前测试私聊模式中使用用户自己的账户，请使用 pixiv login <uid> <password> 指令尝试登陆"
