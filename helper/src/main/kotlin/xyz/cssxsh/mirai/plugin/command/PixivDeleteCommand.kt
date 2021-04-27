3e6e0309689080fb29f86e2302ecb6fd65cf9536
cssxsh <cssxsh@gmail.com>
2021-04-28 07:09:32 +0800

    refactor: PixivDeleteCommand

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivDeleteCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivDeleteCommand.kt
@@ -14,34 +14,36 @@ object PixivDeleteCommand : CompositeCommand(
     overrideContext = PixivCommandArgumentContext
 ) {
 
-    private fun deleteImage(pid: Long) = PixivHelperSettings.imagesFolder(pid).listFiles { file ->
-        file.isFile && file.extension != "json"
-    }?.all { it.delete() }
+    private fun deleteImage(pid: Long): Boolean {
+        return PixivHelperSettings.imagesFolder(pid).listFiles { file ->
+            file.isFile && file.extension != "json"
+        }?.all { it.delete() } ?: true
+    }
 
     @SubCommand
     @Description("删除指定作品")
-    fun ConsoleCommandSender.artwork(pid: Long) {
+    suspend fun ConsoleCommandSender.artwork(pid: Long) {
         logger.info { "作品(${pid})信息将从缓存移除" }
-        useMappers { mappers ->
-            mappers.artwork.deleteByPid(
-                pid = pid,
-                comment = "command delete artwork in ${OffsetDateTime.now()}"
-            )
+        useMappers {
+            it.artwork.deleteByPid(pid = pid, comment = "command delete artwork in ${OffsetDateTime.now()}")
         }
-        logger.verbose { "作品(${pid})图片将删除，结果${deleteImage(pid)}" }
+        logger.info { "作品(${pid})信息将从缓存移除" }
+        sendMessage("作品(${pid})图片将删除，结果${deleteImage(pid)}")
     }
 
     @SubCommand
     @Description("删除指定用户作品")
-    fun ConsoleCommandSender.user(uid: Long) {
+    suspend fun ConsoleCommandSender.user(uid: Long) {
+        val artworks = useMappers { it.artwork.userArtWork(uid) }
         useMappers { mappers ->
-            mappers.artwork.userArtWork(uid).also {
-                logger.verbose { "USER(${uid})共${it.size}个作品需要删除" }
-                mappers.artwork.deleteByUid(uid = uid, comment = "command delete user in ${OffsetDateTime.now()}")
-            }
-        }.forEach {
-            logger.verbose { "作品(${it.pid})图片将删除，结果${deleteImage(it.pid)}" }
+            mappers.artwork.deleteByUid(uid = uid, comment = "command delete user in ${OffsetDateTime.now()}")
         }
+        sendMessage("USER(${uid})共${artworks.size}个作品需要删除")
+        artworks.forEach {
+            logger.info { "作品(${it.pid})信息将从缓存移除" }
+            deleteImage(it.pid)
+        }
+        sendMessage("删除完毕")
     }
 
     private const val OFFSET_MAX = 99_999_999L
@@ -52,21 +54,23 @@ object PixivDeleteCommand : CompositeCommand(
 
     @SubCommand
     @Description("删除小于指定收藏数作品（用于处理漫画作品）")
-    fun ConsoleCommandSender.bookmarks(bookmarks: Long) {
+    suspend fun ConsoleCommandSender.bookmarks(bookmarks: Long) {
         ranges.forEach { interval ->
             logger.verbose { "开始检查[$interval]" }
-            useMappers { mappers ->
-                mappers.artwork.artworks(interval).filter { it.totalBookmarks < bookmarks }.also {
-                    logger.verbose { "{<$bookmarks}(${interval})共${it.size}个作品需要删除" }
-                }.forEach {
-                    logger.info { "作品(${it.pid})信息将从缓存移除" }
+            val artworks = useMappers { mappers ->
+                mappers.artwork.artworks(interval).filter { it.totalBookmarks < bookmarks }
+            }
+            sendMessage("{<$bookmarks}(${interval})共${artworks.size}个作品需要删除")
+            artworks.forEach {
+                logger.info { "作品(${it.pid})信息将从缓存移除" }
+                useMappers { mappers ->
                     mappers.artwork.deleteByPid(
                         pid = it.pid,
                         comment = "command delete bookmarks $bookmarks in ${OffsetDateTime.now()}"
                     )
-                    logger.verbose { "作品(${it.pid})图片将删除，结果${deleteImage(it.pid)}" }
                 }
             }
+            sendMessage("删除完毕")
         }
     }
 }
