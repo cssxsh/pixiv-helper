80daeac3f79981f7eee98617c9f24292100522d2
cssxsh <cssxsh@gmail.com>
2021-04-28 07:09:56 +0800

    refactor: PixivRankCommand

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivRankCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivRankCommand.kt
@@ -6,7 +6,7 @@ import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import xyz.cssxsh.mirai.plugin.PixivHelperPlugin
 import xyz.cssxsh.mirai.plugin.*
-import xyz.cssxsh.mirai.plugin.model.NaviRankRecord
+import xyz.cssxsh.mirai.plugin.model.*
 import xyz.cssxsh.mirai.plugin.tools.NaviRank
 import java.time.Year
 import java.time.YearMonth
@@ -28,33 +28,34 @@ object PixivRankCommand: CompositeCommand(
         }
     }
 
+    private suspend fun NaviRank.rank(year: Year, vararg words: String) =
+        if (words.isNotEmpty()) getTagRank(year = year, words = words) else getAllRank(year = year)
+
+    private suspend fun NaviRank.rank(month: YearMonth, vararg words: String) =
+        if (words.isNotEmpty()) getTagRank(month = month, words = words) else getAllRank(month = month)
+
+    private suspend fun NaviRankAllTime.cached(helper: PixivHelper): ArtWorkInfo {
+        helper.addCacheJob(name = "NAVIRANK[${title}]", reply = false) {
+            getListIllusts(info = records)
+        }
+        return records.cached().random()
+    }
+
     @SubCommand("year", "年", "年榜")
     @Description("获取NaviRank年榜并随机")
-    suspend fun CommandSenderOnMessage<*>.year(year: Year) = sendIllust {
-        NaviRank.getAllRank(year = year).records.also {
-            addCacheJob(name = "NAVIRANK[${year}]", reply = false) {
-                getListIllusts(info = it)
-            }
-        }.cached().random()
+    suspend fun CommandSenderOnMessage<*>.year(year: Year, vararg words: String) = sendIllust {
+        NaviRank.rank(year = year, words = words).cached(helper = this)
     }
 
     @SubCommand("month", "月", "月榜")
     @Description("获取NaviRank月榜并随机")
-    suspend fun CommandSenderOnMessage<*>.month(month: YearMonth) = sendIllust {
-        NaviRank.getAllRank(month = month).records.also {
-            addCacheJob(name = "NAVIRANK[${month}]", reply = false) {
-                getListIllusts(info = it)
-            }
-        }.cached().random()
+    suspend fun CommandSenderOnMessage<*>.month(month: YearMonth, vararg words: String) = sendIllust {
+        NaviRank.rank(month = month, words = words).cached(helper = this)
     }
 
     @SubCommand("tag", "标签")
     @Description("获取NaviRank标签榜并随机")
     suspend fun CommandSenderOnMessage<*>.tag(vararg words: String) = sendIllust {
-        NaviRank.getTagRank(words = words).records.also {
-            addCacheJob(name = "NAVIRANK[${words}]", reply = false) {
-                getListIllusts(info = it)
-            }
-        }.cached().random()
+        NaviRank.getTagRank(words = words).cached(helper = this)
     }
 }
