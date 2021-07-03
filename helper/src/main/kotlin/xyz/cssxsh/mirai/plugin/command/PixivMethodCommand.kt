4a2c06b2e6bd2520361f80f33d02e60da08dce8c
cssxsh <cssxsh@gmail.com>
2021-07-04 00:38:15 +0800

    fix: AuthPixivClient

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivMethodCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivMethodCommand.kt
@@ -5,7 +5,7 @@ import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.message.data.toPlainText
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
 import xyz.cssxsh.mirai.plugin.*
-import xyz.cssxsh.pixiv.sina
+import xyz.cssxsh.pixiv.*
 
 object PixivMethodCommand : CompositeCommand(
     owner = PixivHelperPlugin,
@@ -17,10 +17,14 @@ object PixivMethodCommand : CompositeCommand(
     @SubCommand
     @Description("登录 通过 登录关联的微博")
     suspend fun CommandSenderOnMessage<*>.sina() = withHelper {
-        sina {
-            send {
-                it.inputStream().uploadAsImage(contact) + "请扫码登录关联了Pixiv的微博".toPlainText()
-            }
+        sina { url ->
+            sendMessage(
+                runCatching {
+                    PixivHelperDownloader.downloadImage(url).inputStream().uploadAsImage(contact)
+                }.getOrElse {
+                    url.toString().toPlainText()
+                } + " 请扫码登录关联了Pixiv的微博".toPlainText()
+            )
         }.let {
             "登陆成功，请妥善保管 RefreshToken: ${it.refreshToken}"
         }
