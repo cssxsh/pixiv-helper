ce9e8cdaaa911e9c0d7834f9e887a4266bf9758b
cssxsh <cssxsh@gmail.com>
2021-07-06 00:17:02 +0800

    fix: delete command sender

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivDeleteCommand.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/command/PixivDeleteCommand.kt
@@ -1,7 +1,7 @@
 package xyz.cssxsh.mirai.plugin.command
 
 import net.mamoe.mirai.console.command.CompositeCommand
-import net.mamoe.mirai.console.command.ConsoleCommandSender
+import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.utils.*
 import xyz.cssxsh.mirai.plugin.*
 import xyz.cssxsh.mirai.plugin.data.*
@@ -22,7 +22,7 @@ object PixivDeleteCommand : CompositeCommand(
 
     @SubCommand
     @Description("删除指定作品")
-    suspend fun ConsoleCommandSender.artwork(pid: Long) {
+    suspend fun CommandSender.artwork(pid: Long) {
         logger.info { "作品(${pid})信息将从缓存移除" }
         useMappers {
             it.artwork.deleteByPid(pid = pid, comment = "command delete artwork in ${OffsetDateTime.now()}")
@@ -33,7 +33,7 @@ object PixivDeleteCommand : CompositeCommand(
 
     @SubCommand
     @Description("删除指定用户作品")
-    suspend fun ConsoleCommandSender.user(uid: Long) {
+    suspend fun CommandSender.user(uid: Long) {
         val artworks = useMappers { it.artwork.userArtWork(uid) }
         useMappers { mappers ->
             mappers.artwork.deleteByUid(uid = uid, comment = "command delete user in ${OffsetDateTime.now()}")
@@ -54,7 +54,7 @@ object PixivDeleteCommand : CompositeCommand(
 
     @SubCommand
     @Description("删除小于指定收藏数作品（用于处理漫画作品）")
-    suspend fun ConsoleCommandSender.bookmarks(bookmarks: Long) {
+    suspend fun CommandSender.bookmarks(bookmarks: Long) {
         ranges.forEach { interval ->
             logger.verbose { "开始检查[$interval]" }
             val artworks = useMappers { mappers ->
