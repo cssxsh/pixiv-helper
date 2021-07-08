93c11f716a7c4922a61f320a1fe7873c88d7c403
cssxsh <cssxsh@gmail.com>
2021-07-08 18:12:40 +0800

    fix: eroRandom

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/dao/ArtWorkInfoMapper.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/dao/ArtWorkInfoMapper.kt
@@ -1,5 +1,6 @@
 package xyz.cssxsh.mirai.plugin.dao
 
+import org.apache.ibatis.annotations.Param
 import xyz.cssxsh.mirai.plugin.model.ArtWorkInfo
 
 interface ArtWorkInfoMapper {
@@ -8,13 +9,23 @@ interface ArtWorkInfoMapper {
     fun replaceArtWork(info: ArtWorkInfo)
     fun updateArtWork(info: ArtWorkInfo)
     fun artworks(interval: LongRange): List<ArtWorkInfo>
-    fun findByTag(tag: String, bookmark: Long, fuzzy: Boolean): List<ArtWorkInfo>
+    fun findByTag(
+        @Param("tag") tag: String,
+        @Param("bookmarks") bookmarks: Long,
+        @Param("fuzzy") fuzzy: Boolean
+    ): List<ArtWorkInfo>
+
     fun count(): Long
-    fun deleteByPid(pid: Long, comment: String)
-    fun deleteByUid(uid: Long, comment: String)
+    fun deleteByPid(@Param("pid") pid: Long, @Param("comment") comment: String)
+    fun deleteByUid(@Param("pid") uid: Long, @Param("comment") comment: String)
     fun userArtWork(uid: Long): List<ArtWorkInfo>
     fun userEroCount(): List<Pair<Long, Long>>
-    fun eroRandom(limit: Int): List<ArtWorkInfo>
+    fun eroRandom(
+        @Param("limit") limit: Int,
+        @Param("level") level: Int,
+        @Param("bookmarks") bookmarks: Long
+    ): List<ArtWorkInfo>
+
     fun eroCount(): Long
     fun r18Count(): Long
     fun contains(pid: Long): Boolean
