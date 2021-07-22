7c83dc7917be9c931ba5c58ac5230847a927a964
cssxsh <cssxsh@gmail.com>
2021-07-22 21:24:04 +0800

    style: ...

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivUtils.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivUtils.kt
@@ -285,7 +285,7 @@ internal fun IllustInfo.toTagInfo() = tags.map {
 internal fun UserInfoMapper.add(user: UserBaseInfo) =
     if (findByUid(user.uid) != null) updateUser(user) else replaceUser(user)
 
-internal inline fun IllustInfo.check() = apply {
+internal fun IllustInfo.check() = apply {
     check(user.id != 0L) { "作品已删除或者被限制, Redirect: ${getOriginImageUrls().single()}" }
 }
 
