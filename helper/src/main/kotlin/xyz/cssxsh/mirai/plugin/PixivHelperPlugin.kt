6ab34b17603b3963146ef57019477e149cd25190
cssxsh <cssxsh@gmail.com>
2021-08-06 22:32:40 +0800

    build: 1.1.6

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperPlugin.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperPlugin.kt
@@ -10,7 +10,7 @@ import xyz.cssxsh.mirai.plugin.tools.*
 import java.util.logging.*
 
 object PixivHelperPlugin : KotlinPlugin(
-    JvmPluginDescription("xyz.cssxsh.mirai.plugin.pixiv-helper", "1.1.5") {
+    JvmPluginDescription("xyz.cssxsh.mirai.plugin.pixiv-helper", "1.1.6") {
         name("pixiv-helper")
         author("cssxsh")
     }
