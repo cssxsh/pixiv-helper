363c039f47f09ccd3753f4924efc2a30ffad178d
cssxsh <cssxsh@gmail.com>
2021-08-08 01:26:30 +0800

    fix: mysql sql

--- helper/src/main/resources/xyz/cssxsh/mirai/plugin/model/create.mysql.sql
+++ helper/src/main/resources/xyz/cssxsh/mirai/plugin/model/create.mysql.sql
@@ -1,39 +1,39 @@
 -- Illust Data
 CREATE TABLE IF NOT EXISTS users
 (
-    `uid`     INTEGER      NOT NULL,
-    `name`    NVARCHAR(15) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-    `account` VARCHAR(32)  NOT NULL COLLATE 'ascii_general_ci',
+    `uid`     INTEGER     NOT NULL,
+    `name`    VARCHAR(15) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+    `account` VARCHAR(32) NOT NULL COLLATE 'ascii_general_ci',
     PRIMARY KEY (`uid`)
 );
 CREATE TABLE IF NOT EXISTS artworks
 (
-    `pid`             INTEGER      NOT NULL,
-    `uid`             INTEGER      NOT NULL,
-    `title`           NVARCHAR(32) NOT NULL,
-    `caption`         TEXT         NOT NULL,
-    `create_at`       INTEGER      NOT NULL,
+    `pid`             INTEGER     NOT NULL,
+    `uid`             INTEGER     NOT NULL,
+    `title`           VARCHAR(32) NOT NULL,
+    `caption`         TEXT        NOT NULL,
+    `create_at`       INTEGER     NOT NULL,
     -- page_count max 200
-    `page_count`      SMALLINT     NOT NULL,
+    `page_count`      SMALLINT    NOT NULL,
     -- sanity_level 0 2 4 6 7
-    `sanity_level`    TINYINT      NOT NULL,
-    `type`            TINYINT      NOT NULL,
-    `width`           SMALLINT     NOT NULL,
-    `height`          SMALLINT     NOT NULL,
-    `total_bookmarks` INTEGER      NOT NULL DEFAULT 0,
-    `total_comments`  INTEGER      NOT NULL DEFAULT 0,
-    `total_view`      INTEGER      NOT NULL DEFAULT 0,
-    `age`             TINYINT      NOT NULL DEFAULT 0,
-    `is_ero`          BOOLEAN      NOT NULL DEFAULT FALSE,
-    `deleted`         BOOLEAN      NOT NULL DEFAULT FALSE,
+    `sanity_level`    TINYINT     NOT NULL,
+    `type`            TINYINT     NOT NULL,
+    `width`           SMALLINT    NOT NULL,
+    `height`          SMALLINT    NOT NULL,
+    `total_bookmarks` INTEGER     NOT NULL DEFAULT 0,
+    `total_comments`  INTEGER     NOT NULL DEFAULT 0,
+    `total_view`      INTEGER     NOT NULL DEFAULT 0,
+    `age`             TINYINT     NOT NULL DEFAULT 0,
+    `is_ero`          BOOLEAN     NOT NULL DEFAULT FALSE,
+    `deleted`         BOOLEAN     NOT NULL DEFAULT FALSE,
     PRIMARY KEY (`pid`),
     FOREIGN KEY (`uid`) REFERENCES users (`uid`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS tags
 (
-    `pid`             INTEGER      NOT NULL,
-    `name`            NVARCHAR(30) NOT NULL COLLATE 'utf8mb4_bin',
-    `translated_name` NVARCHAR(64) COLLATE 'utf8mb4_bin',
+    `pid`             INTEGER     NOT NULL,
+    `name`            VARCHAR(30) NOT NULL COLLATE 'utf8mb4_bin',
+    `translated_name` VARCHAR(64) COLLATE 'utf8mb4_bin',
     PRIMARY KEY (`pid`, `name`),
     FOREIGN KEY (`pid`) REFERENCES artworks (`pid`) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -60,27 +60,27 @@ CREATE TABLE IF NOT EXISTS statistic_ero
 );
 CREATE TABLE IF NOT EXISTS statistic_tag
 (
-    `sender`    INTEGER      NOT NULL,
+    `sender`    INTEGER     NOT NULL,
     `group`     INTEGER,
     `pid`       INTEGER,
-    `tag`       NVARCHAR(30) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-    `timestamp` INTEGER      NOT NULL,
+    `tag`       VARCHAR(30) NOT NULL,
+    `timestamp` INTEGER     NOT NULL,
     PRIMARY KEY (`sender`, `timestamp`)
 );
 CREATE TABLE IF NOT EXISTS statistic_search
 (
-    `md5`        NCHAR(32)     NOT NULL COLLATE 'ascii_general_ci',
+    `md5`        CHAR(32)     NOT NULL COLLATE 'ascii_general_ci',
     `similarity` NUMERIC(6, 4) NOT NULL,
     `pid`        INTEGER       NOT NULL,
-    `title`      NVARCHAR(32)  NOT NULL,
+    `title`      VARCHAR(32)   NOT NULL,
     `uid`        INTEGER       NOT NULL,
-    `name`       NVARCHAR(15)  NOT NULL,
+    `name`       VARCHAR(15)   NOT NULL,
     PRIMARY KEY (`md5`)
 );
 CREATE TABLE IF NOT EXISTS statistic_alias
 (
-    `name` NVARCHAR(15) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-    `uid`  INTEGER      NOT NULL,
+    `name` VARCHAR(15) NOT NULL,
+    `uid`  INTEGER     NOT NULL,
     PRIMARY KEY (`name`)
 );
 CREATE TABLE IF NOT EXISTS statistic_task
