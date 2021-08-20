1aea7edb78fa709d7368af61f4e3e38ac85d02e9
cssxsh <cssxsh@gmail.com>
2021-08-20 10:44:27 +0800

    fix: PixivDownloader

--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperDownloader.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperDownloader.kt
@@ -2,13 +2,24 @@ package xyz.cssxsh.mirai.plugin
 
 import io.ktor.http.*
 import xyz.cssxsh.mirai.plugin.data.*
+import xyz.cssxsh.pixiv.*
 import xyz.cssxsh.pixiv.tool.*
+import java.net.Proxy
 
 object PixivHelperDownloader : PixivDownloader(host = PIXIV_HOST, async = 32) {
 
     override val ignore: suspend (Throwable) -> Boolean get() = PixivDownloadIgnore
 
     override val timeout: Long by PixivHelperSettings::download
+
+    @Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+    override val proxy: Proxy? by lazy {
+        if (PixivHelperSettings.proxy.isNotBlank()) {
+            Url(PixivHelperSettings.proxy).toProxy()
+        } else {
+            null
+        }
+    }
 
     override suspend fun <R> downloadImageUrls(
         urls: List<Url>,
