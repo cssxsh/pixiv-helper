960ca009525d5801192e36545a5aab00137c952f
cssxsh <cssxsh@gmail.com>
2021-11-13 21:24:08 +0800

    build: 1.6.8

--- helper/build.gradle.kts
+++ helper/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "xyz.cssxsh.mirai.plugin"
-version = "1.6.7"
+version = "1.6.8"
 
 mirai {
     jvmTarget = JavaVersion.VERSION_11
--- helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperPlugin.kt
+++ helper/src/main/kotlin/xyz/cssxsh/mirai/plugin/PixivHelperPlugin.kt
@@ -17,7 +17,7 @@ import xyz.cssxsh.mirai.plugin.tools.*
 import java.util.logging.*
 
 object PixivHelperPlugin : KotlinPlugin(
-    JvmPluginDescription("xyz.cssxsh.mirai.plugin.pixiv-helper", "1.6.7") {
+    JvmPluginDescription("xyz.cssxsh.mirai.plugin.pixiv-helper", "1.6.8") {
         name("pixiv-helper")
         author("cssxsh")
     }
