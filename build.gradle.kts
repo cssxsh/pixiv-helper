3f45e8812bdacd06744a89db4c676aafecd92536
cssxsh <cssxsh@gmail.com>
2022-07-09 01:22:25 +0800

    update: mirai-hibernate-plugin:2.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,18 +27,18 @@ dependencies {
         exclude("org.javassist")
     }
     compileOnly("javax.validation:validation-api:2.0.1.Final")
-    compileOnly("net.mamoe:mirai-core:2.12.0-RC")
-    compileOnly("net.mamoe:mirai-core-utils:2.12.0-RC")
+    compileOnly("net.mamoe:mirai-core:2.12.0")
+    compileOnly("net.mamoe:mirai-core-utils:2.12.0")
     // dependsOn
-    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.3.0-M2")
-    compileOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.1.1")
-    compileOnly("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.1")
+    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.3.3")
+    compileOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.2.0")
+    compileOnly("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.3")
 
     testImplementation(kotlin("test", "1.6.21"))
     testImplementation("net.mamoe:mirai-slf4j-bridge:1.2.0")
-    testImplementation("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.3.0-M2")
-    testImplementation("xyz.cssxsh.mirai:mirai-selenium-plugin:2.1.1")
-    testImplementation("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.1")
+    testImplementation("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.3.3")
+    testImplementation("xyz.cssxsh.mirai:mirai-selenium-plugin:2.2.0")
+    testImplementation("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.3")
 }
 
 mirai {
