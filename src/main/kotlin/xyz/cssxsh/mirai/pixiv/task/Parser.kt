0093d735a0d00810ad1ffc51f2d15f9342ee0c27
cssxsh <cssxsh@gmail.com>
2022-12-22 11:59:39 +0800

    fix: cron

--- src/main/kotlin/xyz/cssxsh/mirai/pixiv/task/Parser.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/pixiv/task/Parser.kt
@@ -7,14 +7,14 @@ import com.cronutils.model.time.*
 import com.cronutils.parser.*
 import java.util.*
 
-internal const val CRON_TYPE_KEY = "xyz.cssxsh.mirai.pixiv.cron.type"
+internal const val CRON_TYPE_KEY = "xyz.cssxsh.mirai.cron.type"
 
 public val DefaultCronParser: CronParser by lazy {
     val type = CronType.valueOf(System.getProperty(CRON_TYPE_KEY, CronType.QUARTZ.name))
     CronParser(CronDefinitionBuilder.instanceDefinitionFor(type))
 }
 
-internal const val CRON_LOCALE_KEY = "xyz.cssxsh.mirai.pixiv.cron.locale"
+internal const val CRON_LOCALE_KEY = "xyz.cssxsh.mirai.cron.locale"
 
 public val DefaultCronDescriptor: CronDescriptor by lazy {
     val locale = System.getProperty(CRON_LOCALE_KEY)?.let { Locale.forLanguageTag(it) } ?: Locale.getDefault()
